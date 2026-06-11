a1e5694b9331c3ca5ab88524152960dc6b05bc9c
felipefonseca2079 <fonsecafelipe295@gmail.com>
2026-06-11 20:36:09 -0300

    Update JogadorHumano.java

--- src/main/java/JogadorHumano.java
+++ src/main/java/JogadorHumano.java
@@ -1,1 +1,14 @@
+public abstract class Jogador {
+    private String nome;
 
+    public Jogador(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    // Método abstrato que as subclasses serão obrigadas a implementar
+    public abstract void jogar();
+}
