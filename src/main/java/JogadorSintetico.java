1eb6fd60a146f0f466506a0e9293ac1863a76126
felipefonseca2079 <fonsecafelipe295@gmail.com>
2026-06-11 20:38:10 -0300

    Update JogadorSintetico.java

--- src/main/java/JogadorSintetico.java
+++ src/main/java/JogadorSintetico.java
@@ -1,1 +1,11 @@
+// Jogador controlado pela máquina (IA / Sintético)
+public class JogadorSintetico extends Jogador {
+    public JogadorSintetico(String nome) {
+        super(nome);
+    }
 
+    @Override
+    public void jogar() {
+        System.out.println("Jogador Sintético (" + getNome() + ") calculou e fez uma jogada.");
+    }
+}
