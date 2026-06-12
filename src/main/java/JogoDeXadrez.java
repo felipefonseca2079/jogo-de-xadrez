c6adee5aefceab2fce7651c8ff9376a1849136c7
felipefonseca2079 <fonsecafelipe295@gmail.com>
2026-06-11 21:09:44 -0300

    Update JogoDeXadrez.java

--- src/main/java/JogoDeXadrez.java
+++ src/main/java/JogoDeXadrez.java
@@ -1,7 +1,18 @@
-class JogoDeXadrez implements Jogo
-
-public class ImprimirNome {
+class JogoDeXadrez implements Jogo{
     public static void main(String[] args) {
         System.out.println("Felipe Fonseca Sanches");
     }
 }
+    public void iniciar() {
+        Tabuleiro tabuleiro = new Tabuleiro();
+        Jogador jogador1 = new JogadorHumano("Humano");
+        Jogador jogador2 = new JogadorSintetico("Sintetico");
+
+        while (!tabuleiro.acabouOJogo()) {
+            jogador1.jogar();
+            if (tabuleiro.acabouOJogo()) {
+                break;
+            }
+            jogador2.jogar();
+        }
+    }
