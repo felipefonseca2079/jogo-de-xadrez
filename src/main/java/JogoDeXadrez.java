fcd24da767d68edb5c957218203f9013f431ae8d
felipefonseca2079 <fonsecafelipe295@gmail.com>
2026-06-11 21:18:51 -0300

    Update JogoDeXadrez.java

--- src/main/java/JogoDeXadrez.java
+++ src/main/java/JogoDeXadrez.java
@@ -15,4 +15,27 @@ class JogoDeXadrez implements Jogo{
             }
             jogador2.jogar();
         }
+    }class JogoDeXadrez implements Jogo {
+
+    @Override
+    public void iniciar() {
+        Tabuleiro tabuleiro = new Tabuleiro();
+        Jogador jogador1 = new JogadorHumano("Humano", 'b');
+        Jogador jogador2 = new JogadorSintetico("Sintetico", 'p');
+
+        while (!tabuleiro.acabouOJogo()) {
+            jogador1.jogar(tabuleiro, "Peão", 12);
+            if (tabuleiro.acabouOJogo()) {
+                break;
+            }
+            jogador2.jogar(tabuleiro, "Cavalo", 24);
+        }
     }
+
+    public static void main(String[] args) {
+        JogoDeXadrez partida = new JogoDeXadrez();
+        partida.iniciar();
+        System.out.println("SEU NOME COMPLETO"); 
+    }
+}
+
