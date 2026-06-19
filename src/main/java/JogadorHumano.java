9e2c080950e0a12d1cd62dc4208242c757309f52
felipefonseca2079 <fonsecafelipe295@gmail.com>
2026-06-18 21:20:09 -0300

    Update JogadorHumano.java

--- src/main/java/JogadorHumano.java
+++ src/main/java/JogadorHumano.java
@@ -1,12 +1,9 @@
 public class JogadorHumano extends Jogador {
 
-    // Ajustado para receber nome e cor
     public JogadorHumano(String nome, char cor) {
         super(nome, cor); 
     }
 
-    // Assinatura exata exigida pela JogadorInterface / Jogador
-    @Override
     public boolean jogar(Tabuleiro tabuleiro, String nomeDaPeca, int numeroDaCasa) {
         System.out.println("Jogador Humano fez sua jogada.");
         return true;
