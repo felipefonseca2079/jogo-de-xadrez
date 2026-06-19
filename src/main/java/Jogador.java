54b00db68758797587891cc9ed3fb4ba73696001
felipefonseca2079 <fonsecafelipe295@gmail.com>
2026-06-18 21:12:45 -0300

    Update Jogador.java

--- src/main/java/Jogador.java
+++ src/main/java/Jogador.java
@@ -1,20 +1,4 @@
-public abstract class Jogador {
-    private char simbolo;
-    private String nome;
 
-    public Jogador(String nome, char simbolo) {
-        this.nome = nome;
-        this.simbolo = simbolo;
-    }
-
-    public char getSimbolo() {
-        return this.simbolo;
-    }
-
-    public String getNome() {
-        return this.nome;
-    }
-}
 public abstract class Jogador implements JogadorInterface {
     private String nome;
     private char cor;
