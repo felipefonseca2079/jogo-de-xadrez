b1f869d6c3bd3934bdb208a904996730f5ddb043
felipefonseca2079 <fonsecafelipe295@gmail.com>
2026-06-25 20:17:51 -0300

    Update Tabuleiro.java

--- src/main/java/Tabuleiro.java
+++ src/main/java/Tabuleiro.java
@@ -1,26 +1,32 @@
 public class Tabuleiro {
-    private Peca[][] casas; 
+    private String[][] casas = new String[8][8]; 
 
     public Tabuleiro() {
-        this.casas = new Peca[8][8]; 
-        inicializarTabuleiro();
-    }
-
-    private void inicializarTabuleiro() {
-        casas[0][0] = new Torre(1, 'B'); 
-        casas[0][1] = new Cavalo(1, 'B');
-   }
-
-    public void mostrarTabuleiro() {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                if (casas[i][j] != null) {
-             
-                    System.out.print(casas[i][j].getNome() + " "); 
-                } else {
-             
-                    System.out.print("[ ] ");
-                }
+                casas[i][j] = "   ";
+            }
+        }
+        colocarPecas();
+        mostrar();
+    }
+
+    private void colocarPecas() {
+        Peca reiBranco = new Rei(0, 'b');
+        Peca reiPreto = new Rei(0, 'p');
+        Peca torreBranca1 = new Torre(1, 'b');
+        Peca cavaloPreto1 = new Cavalo(1, 'p');
+
+        casas[0][4] = reiPreto.getNome();
+        casas[7][4] = reiBranco.getNome();
+        casas[7][0] = torreBranca1.getNome();
+        casas[0][1] = cavaloPreto1.getNome();
+    }
+
+    public void mostrar() {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                System.out.print("[" + casas[i][j] + "]");
             }
             System.out.println();
         }
