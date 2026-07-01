eddb9a9c2b87a85aaae76b2c4ef91481be29539e
Flávio Gomes da Silva Lisboa <flavio.lisboa@fgsl.eti.br>
2026-06-30 21:32:06 -0300

    Update JUnit imports to use Jupiter API

--- src/test/java/JogoDeXadrezTest.java
+++ src/test/java/JogoDeXadrezTest.java
@@ -1,7 +1,7 @@
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions,assertTrue;
+import org.junit.jupiter.api.Test;
 
 public class JogoDeXadrezTest {
 
